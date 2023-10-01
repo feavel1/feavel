c1800f9ed2ebe0c94d4aac6cf8cd28af95a2ccca
Baby Feavel <feavel@qq.com>
2023-10-01 16:45:59 +0800

    try this

--- src/routes/api/generate/+server.ts
+++ src/routes/api/generate/+server.ts
@@ -80,7 +80,7 @@ export const POST: RequestHandler = async ({ request: req }) => {
 		// Respond with the stream
 		return new StreamingTextResponse(stream);
 	} catch (err) {
-		alert('There was an error with you api key, check your openAI account');
+		throw err;
 		return json({ error: 'There was an error processing your request' }, { status: 500 });
 	}
 };
