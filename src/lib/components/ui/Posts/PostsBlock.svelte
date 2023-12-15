60acc946b6ef257fadbc93fef8b3035201b87cbb
Baby Feavel <feavel@qq.com>
2023-12-15 16:02:03 +0800

    breaking changes

---
+++ src/lib/components/ui/Posts/PostsBlock.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import PostPlaceHolder from '$lib/components/ui/PostPlaceHolder.svelte';
+	import PostCard from '$lib/components/ui/Posts/PostCard.svelte';
+	import { Paginator, type PaginationSettings } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
+	export let supabase: any;
+
+	let isLoading = false;
+	let source: string | any[] = [];
+	let cachedPosts: any[] = [];
+
+	let paginationSettings: PaginationSettings = {
+		amounts: [],
+		limit: 6,
+		page: 0,
+		size: source.length
+	};
+
+	onMount(async () => {
+		await getTotal();
+		getPosts();
+	});
+
+	async function getTotal() {
+		try {
+			isLoading = true;
+			const { data: totalCountData, error: totalCountErr } = await supabase
+				.from('posts')
+				.select('count', { count: 'exact' })
+				.eq('publicVisibility', 'true');
+
+			if (totalCountData) {
+				paginationSettings.size = totalCountData[0].count;
+			}
+		} catch (error) {
+			console.error('Error fetching total count:', error);
+		} finally {
+			isLoading = false;
+		}
+	}
+
+	async function getPosts() {
+		try {
+			isLoading = true;
+			const startRange = paginationSettings.page * paginationSettings.limit;
+
+			// Adjust endRange to avoid exceeding the total count
+			let endRange = startRange + paginationSettings.limit - 1;
+			if (startRange >= paginationSettings.size) {
+				// If startRange is beyond the total count, set endRange to startRange
+				endRange = startRange;
+			} else if (endRange >= paginationSettings.size) {
+				// If endRange exceeds the total count, adjust it to the last index
+				endRange = paginationSettings.size - 1;
+			}
+
+			// Check if the posts for the current page are already cached
+			if (cachedPosts[startRange] !== undefined && cachedPosts[endRange] !== undefined) {
+				source = cachedPosts.slice(startRange, endRange + 1);
+			} else {
+				const { data: post_data, error: post_data_err } = await supabase
+					.from('posts')
+					.select(`id, created_at, user_id, title, posts_tags_rel(post_tags(tag_name))`)
+					.eq('publicVisibility', 'true')
+					.range(startRange, endRange);
+
+				if (post_data) {
+					source = [...post_data];
+					// Update the cached posts array with the new data
+					cachedPosts.splice(startRange, paginationSettings.limit, ...post_data);
+				} else {
+					console.error('Error fetching posts:', post_data_err);
+				}
+			}
+		} catch (error) {
+			console.error('Error fetching posts:', error);
+		} finally {
+			isLoading = false;
+		}
+	}
+</script>
+
+<div class="grid grid-cols-1 grid-rows-3 lg:grid-cols-2 gap-4 first:row-span-1">
+	{#if isLoading}
+		<PostPlaceHolder />
+	{:else}
+		{#each source as post}
+			<PostCard {post} />
+		{/each}
+	{/if}
+</div>
+
+<Paginator
+	class="mx-auto w-min"
+	showFirstLastButtons={true}
+	showPreviousNextButtons={true}
+	bind:settings={paginationSettings}
+	on:page={getPosts}
+/>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import PostPlaceHolder from '$lib/components/ui/PostPlaceHolder.svelte';
-	import PostCard from '$lib/components/ui/Posts/PostCard.svelte';
+	import PostsBlock from '$lib/components/ui/Posts/PostsBlock.svelte';
 	import {
-		Paginator,
 		type PaginationSettings,
 		type PopupSettings,
 		Autocomplete,
@@ -15,8 +13,8 @@
 	let { supabase } = data;
 	$: ({ supabase } = data);
 
-	let isLoading = false;
 	let source: string | any[] = [];
+	let isLoading = false;
 	let cachedPosts: any[] = [];
 
 	let paginationSettings: PaginationSettings = {
@@ -28,67 +26,7 @@
 
 	onMount(async () => {
 		getTags();
-		await getTotal();
-		getPosts();
 	});
-
-	async function getTotal() {
-		try {
-			isLoading = true;
-			const { data: totalCountData, error: totalCountErr } = await supabase
-				.from('posts')
-				.select('count', { count: 'exact' })
-				.eq('publicVisibility', 'true');
-
-			if (totalCountData) {
-				paginationSettings.size = totalCountData[0].count;
-			}
-		} catch (error) {
-			console.error('Error fetching total count:', error);
-		} finally {
-			isLoading = false;
-		}
-	}
-
-	async function getPosts() {
-		try {
-			isLoading = true;
-			const startRange = paginationSettings.page * paginationSettings.limit;
-
-			// Adjust endRange to avoid exceeding the total count
-			let endRange = startRange + paginationSettings.limit - 1;
-			if (startRange >= paginationSettings.size) {
-				// If startRange is beyond the total count, set endRange to startRange
-				endRange = startRange;
-			} else if (endRange >= paginationSettings.size) {
-				// If endRange exceeds the total count, adjust it to the last index
-				endRange = paginationSettings.size - 1;
-			}
-
-			// Check if the posts for the current page are already cached
-			if (cachedPosts[startRange] !== undefined && cachedPosts[endRange] !== undefined) {
-				source = cachedPosts.slice(startRange, endRange + 1);
-			} else {
-				const { data: post_data, error: post_data_err } = await supabase
-					.from('posts')
-					.select(`id, created_at, user_id, title, posts_tags_rel(post_tags(tag_name))`)
-					.eq('publicVisibility', 'true')
-					.range(startRange, endRange);
-
-				if (post_data) {
-					source = [...post_data];
-					// Update the cached posts array with the new data
-					cachedPosts.splice(startRange, paginationSettings.limit, ...post_data);
-				} else {
-					console.error('Error fetching posts:', post_data_err);
-				}
-			}
-		} catch (error) {
-			console.error('Error fetching posts:', error);
-		} finally {
-			isLoading = false;
-		}
-	}
 
 	async function getTags() {
 		try {
@@ -146,7 +84,7 @@
 				use:popup={popupSettings}
 				placeholder="Search..."
 			/>
-			<button class="variant-filled-secondary">Search</button>
+			<button class="variant-filled-secondary">Search posts</button>
 		</div>
 		<div
 			class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto z-50"
@@ -168,20 +106,4 @@
 	</div>
 </div>
 
-<div class="grid grid-cols-1 grid-rows-3 lg:grid-cols-2 gap-4 first:row-span-1">
-	{#if isLoading}
-		<PostPlaceHolder />
-	{:else}
-		{#each source as post}
-			<PostCard {post} />
-		{/each}
-	{/if}
-</div>
-
-<Paginator
-	class="mx-auto w-min"
-	showFirstLastButtons={true}
-	showPreviousNextButtons={true}
-	bind:settings={paginationSettings}
-	on:page={getPosts}
-/>
+<PostsBlock {supabase} />
