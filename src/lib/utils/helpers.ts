65dec207fd10ea28a9453c19cc28febfc67fe8c9
Baby Feavel <feavel@qq.com>
2024-02-16 22:11:33 +0800

    fix url

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -7,7 +7,6 @@ import { PUBLIC_SITE_URL } from '$env/static/public';
 export const getURL = () => {
 	let url =
 		PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-		process?.env?.PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
 		'http://localhost:5173/';
 	// Make sure to include `https://` when not localhost.
 	url = url.includes('http') ? url : `https://${url}`;
