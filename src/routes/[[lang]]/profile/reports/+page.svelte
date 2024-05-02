fbf100c6f9bfa54744c834fc6303244e59a11896
Baby Feavel <feavel@qq.com>
2024-05-02 18:16:49 +0800

    cool

--- src/routes/[[lang]]/profile/reports/+page.svelte
+++ src/routes/[[lang]]/profile/reports/+page.svelte
@@ -21,7 +21,7 @@
 				class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 			>
 				<LucidePlus class="-ml-0.5 mr-1.5 h-5 w-5" />
-				Create New Order
+				{m.create_new_order()}
 			</a>
 		</div>
 	</div>
@@ -30,7 +30,7 @@
 		<div class="">
 			<div class="mx-auto max-w-7xl sm:px-2 lg:px-8">
 				<div class="mx-auto max-w-2xl px-4 lg:max-w-4xl lg:px-0">
-					<h1 class="text-2xl font-bold tracking-tight sm:text-3xl">Reports</h1>
+					<h1 class="text-2xl font-bold tracking-tight sm:text-3xl">{m.reports()}</h1>
 					<p class="mt-2 text-sm text-gray-500">
 						Check the status of recent orders, manage returns, and discover similar products.
 					</p>
